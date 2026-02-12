8cba6ae65c444174539c7c8ac23e1909a2cd46d3
Epshita Sharma <epshitasharma27@gmail.com>
2026-02-12 08:54:29 +0530

    Add files via upload

---
+++ day12.c
@@ -0,0 +1,35 @@
+//  Write a program to check whether a given matrix is symmetric. A matrix is said to be symmetric if it is a square matrix and is equal to its transpose (i.e., element at position [i][j] is equal to element at position [j][i] for all valid i and j).
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int a[10][10];
+    int flag = 1;
+
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag == 1)
+        printf("Matrix is Symmetric");
+    else
+        printf("Matrix is NOT Symmetric");
+
+    return 0;
+}
