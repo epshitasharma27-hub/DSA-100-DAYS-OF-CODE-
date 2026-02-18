d4cd75cb782647f45a283e8a169fffc85d44179b
Epshita Sharma <epshitasharma27@gmail.com>
2026-02-18 16:53:14 +0530

    Add files via upload

---
+++ day18.c
@@ -0,0 +1,47 @@
+//Given an array of integers, rotate the array to the right by k positions.
+
+
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int arr[], int n, int k) {
+    k = k % n;
+    reverse(arr, 0, n - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+}
+
+int main() {
+    int n, k;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    rotate(arr, n, k);
+
+    printf("Array after rotation:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
